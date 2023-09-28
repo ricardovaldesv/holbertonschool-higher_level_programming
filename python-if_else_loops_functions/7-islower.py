16d5183abf616915bc040427e193db62198500ff
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-28 08:05:01 -0700

    task 7 v2

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,8 @@
-def islower(c):
+def islower(c):    # checks for lowercase character
+    """Returns True if c is lowercase.
+
+    Returns False otherwise.
+    """
     dec = ord(c)
     if dec >= 97 and dec <= 122:
         return True
