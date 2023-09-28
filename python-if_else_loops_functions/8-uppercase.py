e0491e004c2b00f6ab8298b5ae4725f35e9f6a38
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-28 09:57:01 -0700

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):    #prints a string in uppercase followed by a new line.
+    """Prints a string in uppercase followed by a new line."""
+    
+    lenght = len(str)
+    for i in range(0, lenght):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            c = chr(ord(str[i]) -32)
+        else: 
+            c = str[i]
+        print("{}".format(c), end='')
+    print("")    
