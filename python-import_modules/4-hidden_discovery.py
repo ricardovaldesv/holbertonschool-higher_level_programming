e2365f72e56d47cd38c79d3e869f6f3d5567706f
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-29 12:20:24 -0700

    task 4 v1

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import hidden_4
+if __name__ == "__main__":
+
+    for name in dir(hidden_4):
+        if name[0] == "_" and name[1] == "_":
+            continue
+        else:
+            print(name)
