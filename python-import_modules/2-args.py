0c21623e0c3868dca5c0efd0c2735ba2116ebd81
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-29 08:54:35 -0700

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+def func():
+    arguments = len(sys.argv)
+    if arguments == 2:
+        print("{0} {1}".format(arguments - 1, 'argument:'))
+    else:
+        print("{0} {1}".format(arguments - 1, 'arguments:'))
+
+    for i in range(1, arguments):
+        print("{0}: {1}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    func()
