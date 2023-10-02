5ad5c602f70e30773674300ab3d31a4e35397443
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-02 15:33:08 -0700

    task 0

---
+++ python-data_structures/0-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5, 6 ,7]
+print_list_integer(my_list)
---
+++ python-data_structures/README.md
@@ -0,0 +1,4 @@
+# Python - Data Structures: Lists, Tuples
+
+## 0. Print a list of integers
+
