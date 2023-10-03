2f711d6b477c082b3e0078b1c723d9ec2d6e099a
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-03 12:35:36 -0700

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print('{}'.format(''))
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if j == len(matrix[0]) - 1:
+                    print('{}'.format(matrix[i][j]))
+                else:
+                    print('{} '.format(matrix[i][j]), end='') 
