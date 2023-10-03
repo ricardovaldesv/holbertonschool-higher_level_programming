fe9310bd7908d45c43afc9c3410cb5f21b3ccb34
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-03 12:44:35 -0700

    task 6 v2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print('{}'.format(''))
-    else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if j == len(matrix[0]) - 1:
-                    print('{}'.format(matrix[i][j]))
-                else:
-                    print('{} '.format(matrix[i][j]), end='') 
+    flag = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if j == len(matrix[0]) - 1:
+                print('{}'.format(matrix[i][j]))
+            else:
+                print('{} '.format(matrix[i][j]), end='')
+            flag = 1
+    if flag == 0:
+        print('{}'.format('d'))
