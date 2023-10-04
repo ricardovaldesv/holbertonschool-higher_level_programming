7691162660a78b061c912b77d2a57778204898df
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-04 07:53:56 -0700

    task 6 refactor v

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,4 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print('{:d} '.format(matrix[i][j]), end='')
     if flag == 0:
-        print('{:s}'.format(""))
+        print("{}".format(""))
