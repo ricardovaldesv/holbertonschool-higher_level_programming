521f5a9e8d80fde9d86ed011066ef1357668728e
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-04 06:52:51 -0700

    task 6 refactor

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     flag = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if j == len(matrix[0]) - 1:
+            if j == len(matrix[i]) - 1:
                 print('{}'.format(matrix[i][j]))
             else:
                 print('{} '.format(matrix[i][j]), end='')
