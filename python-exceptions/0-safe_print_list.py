965b7f7c4413a075524d2ccd24ff68c1a05e2794
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-06 11:20:13 -0700

    task 0 v4

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        if x is 0:
+        if x == 0:
             return 0
         else:
-            for i in range(x):
+            for i in range(0, x):
                 print('{}'.format(my_list[i]), end='')
     except IndexError:
         return i
