977f6ae58f7dd3a96b66e32e300fb5a7ad8f76f2
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-06 11:08:03 -0700

    task 0 v3

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        if my_list is None or x is 0:
+        if x is 0:
             return 0
         else:
             for i in range(x):
