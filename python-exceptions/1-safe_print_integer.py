22dcb12fc626f83759a2372a8a7fcf317dd6fb60
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-06 13:09:08 -0700

    task 1 v3

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is None:
-            return 0
-        else:
-            print('{:d}'.format(value))
-            return True
-    except Execption as ex:
+        print('{:d}'.format(value))
+        return True
+    except Exception as ex:
         return False
