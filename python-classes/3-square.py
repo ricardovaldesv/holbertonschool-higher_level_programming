35e1e0d0b27512f0965c75d994007323456f9999
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-10 21:34:35 +0000

    task 3 v1

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Square that defines a square by: (based on 0-square.py)."""
+
+
+class Square:
+    """Private instance attribute: size."""
+    def __init__(self, size=0):
+        self.__size = int(size)
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        return self.__size **2
