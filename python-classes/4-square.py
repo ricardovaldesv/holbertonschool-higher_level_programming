fbdae84a81fa81f1db13356c420f81ea4bbd3ed3
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-10 22:20:10 +0000

    task 4 v1

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,8 +11,18 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
     
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value    
+    
     def area(self):
         return self.__size **2
-    def size(self, value):
-        value = self.value
-        return value
