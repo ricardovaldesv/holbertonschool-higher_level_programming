7e4b125fa51dbffeda59e689b538fd958e7e519b
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-12 22:58:23 +0000

    task 9 v2

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
