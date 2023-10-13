27b62f36fbcd34d0e743d484a39058ddb598139c
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-13 20:09:24 +0000

    task 0 v2

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(1, 'sd'))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
+print(add_integer(float('nan')))
+
 try:
     print(add_integer(4, "School"))
 except Exception as e:
