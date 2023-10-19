82fb48fc89d1604580071cecf3fbb6a84330ca54
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-19 22:06:57 +0000

    task 8 v2

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,36 +1,8 @@
 #!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """This file contain a class empty"""
-
-
-class BaseGeometry:
-    """class empty
-    """
-    def area(self):
-        """function that raises an Exception with the message area()
-            is not implemented You are not allowed to import any module
-        Args:
-            emty
-        Return:
-            Nothing
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """function that raises an Exception with the message area()
-            is not implemented
-            You are not allowed to import any module
-            Args:
-                emty
-            Return:
-                Nothing
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""This file contain a class Rectangle that inherits of BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
