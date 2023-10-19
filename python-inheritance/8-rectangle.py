7bcb5c2867c27c765425a2b6342f6ef80f4600c1
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-19 21:48:59 +0000

    task 8 v0

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This file contain a class empty"""
+
+
+class BaseGeometry:
+    """class empty
+    """
+    def area(self):
+        """function that raises an Exception with the message area()
+            is not implemented You are not allowed to import any module
+        Args:
+            emty
+        Return:
+            Nothing
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that raises an Exception with the message area()
+            is not implemented
+            You are not allowed to import any module
+            Args:
+                emty
+            Return:
+                Nothing
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = int(width)
+        self.__height = int(height)
