56e05883f3021f38b9fda223da41263b712c6082
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-19 17:50:32 +0000

    task 4 v0

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This file contain a function"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance of
+        a class that inherited (directly or indirectly) from the specified
+        class ; otherwise False.
+        Args:
+            obj is the object
+            a_class is the class
+        Return:
+            True and false
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
