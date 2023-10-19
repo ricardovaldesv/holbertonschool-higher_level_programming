c1c859df3e3944784c3a4f301e5d08884741a222
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-19 20:49:17 +0000

    task 7 v3

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -6,4 +6,17 @@ bg = BaseGeometry()
 bg.integer_validator("my_int", 12)
 bg.integer_validator("width", 89)
 
-bg.integer_validator("name", "John")
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
