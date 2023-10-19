b6b7c761f1fbd7639a05c7e7e3288ab1d3e85379
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-19 19:03:20 +0000

    task 6 v0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This file contain a class empty"""
+
+
+class BaseGeometry:
+    """class empty
+    """
+    def area(self):
+        """function that raises an Exception with the message area() is not implemented
+            You are not allowed to import any module
+        Args:
+            emty
+        Return:
+            Nothing
+    """
+        raise Exception("area() is not implemented")
