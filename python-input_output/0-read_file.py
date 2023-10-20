21e2a9969e7375f0fb98472cb3eb881572a5fc1c
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-20 20:49:46 +0000

    task 0 v2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Function that open a text file"""
+
 
 def read_file(filename=""):
+    """function that open a text file
+        Args:
+            name of file
+        Return:
+            Nothing
+        """
     with open(filename, encoding="utf-8") as file:
         file1 = file.read()
         for line in file1:
