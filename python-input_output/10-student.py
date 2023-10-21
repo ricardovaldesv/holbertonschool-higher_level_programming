2e22d9fbda238024021b87316c471475da2aaac6
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-21 02:08:10 +0000

    task 10 v1

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""Module defines a student by first_name, last_name, age
+"""
+
+
 class Student:
+    """class defines a student by first_name, last_name, age
+    """
     def __init__(self, first_name, last_name, age):
+        """function
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """function
+        """
         if attrs is None:
             return self.__dict__
         selected_attrs = {}
