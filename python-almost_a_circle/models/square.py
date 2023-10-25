4df40e9620588a1b58f87d15aec38a126afdf289
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-25 20:57:20 +0000

    task 10 V1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,15 +8,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Calls the constructor of the base class (Rectangle)"""
         super().__init__(size, size, x, y, id)
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size):
-        self.__size = size
         self.__width = size
         self.__height = size
 
