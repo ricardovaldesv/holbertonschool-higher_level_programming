d2cc9d6d8729318b03899d135a9b0b40be3035cf
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-25 21:01:10 +0000

    task 10 V3

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,10 +6,9 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        """Calls the constructor of the base class (Rectangle)"""
+        """Calls the constructor of the base class (Rectangle)
+        """
         super().__init__(size, size, x, y, id)
-        self.__width = size
-        self.__height = size
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
