2d006d9e7f0db16e3279cd37aa5f3b4f6a9e534b
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-26 17:58:11 +0000

    task 16 v0

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,3 +26,18 @@ class Base:
         else:
             list_dictionaries = "[]"
             return list_dictionaries
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+
+        dictionary1 = list_objs[0].to_dictionary()
+        json_dictionary1 = Base.to_json_string([dictionary1])
+
+        dictionary2 = list_objs[1].to_dictionary()
+        json_dictionary2 = Base.to_json_string([dictionary2])
+
+        filename = cls.__name__
+
+        with open(filename, mode='w', encoding='utf-8') as a_file:
+            return a_file.write("[" + (str(dictionary1))
+                                + ", " + str(dictionary2) + "]")
