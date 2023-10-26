a5448783f9a55f90e165627c00ae62ca4f541380
ricardovaldesv <ricardovaldes@icloud.com>
2023-10-26 19:25:54 +0000

    task 16 v1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,15 +29,16 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """save_to_file method that writes the json string representation of
+        list_obts to a file.
 
-        dictionary1 = list_objs[0].to_dictionary()
-        json_dictionary1 = Base.to_json_string([dictionary1])
-
-        dictionary2 = list_objs[1].to_dictionary()
-        json_dictionary2 = Base.to_json_string([dictionary2])
-
-        filename = cls.__name__
-
-        with open(filename, mode='w', encoding='utf-8') as a_file:
-            return a_file.write("[" + (str(dictionary1))
-                                + ", " + str(dictionary2) + "]")
+        Args:
+            list_objs ([list of objects]): list of cls instances
+        """
+        list_dicts_python = []
+        class_name = cls.__name__ + ".json"
+        if list_objs is not None:
+            for lst in list_objs:
+                list_dicts_python.append(lst.to_dictionary())
+        with open(class_name, "w+", encoding="utf-8") as a_file:
+            a_file.write(cls.to_json_string(list_dicts_python))
