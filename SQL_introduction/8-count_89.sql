cb754b8051afeec5de2b5947d0d40db888c75670
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-22 20:23:28 +0000

    task 8 v1

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
-SELECT COUNT(*) AS count_of_records_with_id_89
+SELECT COUNT(*)
 FROM first_table
 WHERE id = 89;
