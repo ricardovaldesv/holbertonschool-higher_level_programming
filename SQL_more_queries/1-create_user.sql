8a831136f0d27b72fc91674e809dfb8ac1801e5d
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-24 20:28:59 +0000

    task 1 v3

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user user_0d_1.
 --  User_0d_1 have all privileges
 --  The user_0d_1 password  be set to user_0d_1_pwd
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
