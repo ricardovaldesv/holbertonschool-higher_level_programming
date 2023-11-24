50fc06702496d3bf2c5345680b17b06670986a87
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-24 20:29:43 +0000

    task 1 v4

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 --  User_0d_1 have all privileges
 --  The user_0d_1 password  be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
