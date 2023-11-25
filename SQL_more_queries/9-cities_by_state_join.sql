f5244ca0ea67da3a535a60c1c8f3517b97a2b7b7
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-25 15:38:03 +0000

    task 9 v0

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- lists all cities contained in the database hbtn_0d_usa.
+--      Display: cities.id - cities.name - states.name
+--      Results  be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
