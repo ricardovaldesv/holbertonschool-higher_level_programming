03b2d454b80ab471eb32362440894d1edd9c4335
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-28 23:50:34 +0000

    task 0 v0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+import sys
+import MySQLdb
+
+def list_states(username, password, database):
+    db = MySQLdb.connect(user=username, passwd=password, db=database, host='localhost', port=3306)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY id"
+    cursor.execute(query)
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+    else:
+        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+        list_states(username, password, database)
