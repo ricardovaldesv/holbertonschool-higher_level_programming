71d26b566bb6aea8c142dc1381789f03d668d727
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-29 19:32:07 +0000

    task 6 v2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """contains table states"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
