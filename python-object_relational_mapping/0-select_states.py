32fbd51964e4cfa6798dbc4260ad97cc128991ff
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-29 00:30:27 +0000

    task 0 v4

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,12 @@
 import sys
 import MySQLdb
 
+
 def list_states(username, password, database):
     """Represent a Rectangle."""
 
-    db = MySQLdb.connect(user=username, passwd=password, db=database, host='localhost', port=3306)
+    db = MySQLdb.connect(user=username, passwd=password, db=database,
+                         host='localhost', port=3306)
     cursor = db.cursor()
 
     query = "SELECT * FROM states ORDER BY states.id"
@@ -18,6 +20,7 @@ def list_states(username, password, database):
 
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
