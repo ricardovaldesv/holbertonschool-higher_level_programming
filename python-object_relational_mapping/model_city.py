e6a4a09215b79b061f21965c6f4de1269f23a8f4
ricardovaldesv <ricardovaldes@icloud.com>
2023-11-30 01:03:25 +0000

    task 14 v1

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
+
 class City(Base):
     """contains table cities"""
     __tablename__ = 'cities'
